2f03579943c4a386d48412b223bad5e29dad60df
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-29 22:17:20 +0300

    point

---
+++ intersectionPoint/Program.cs
@@ -0,0 +1,37 @@
+﻿System.Console.WriteLine("Нахождение точки пересечения между двумя прямыми");
+
+System.Console.WriteLine("Введите коэффицент x первого уравнения");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите коэффицент x второго уравнения");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите коэффицент b первого уравнения");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите коэффицент b второго уравнения");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+
+int firstResultX = x1 - x2;
+int resultB = b1 - b2;
+float resultX = 0;
+
+if(firstResultX == 0  || resultB == 0)
+{
+     resultX = 0;
+}
+else
+{
+     resultX = resultB / firstResultX;
+}
+
+System.Console.WriteLine(resultX + "X");
+
+float resultY = x1 * resultX + b1;
+
+System.Console.WriteLine("Точка пересечения двуъ прямых = " +"("+resultX+"; "+resultY+")");
+
+
+
+
