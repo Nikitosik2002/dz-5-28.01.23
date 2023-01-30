8d9d74ba3cf024d8ac77b0abc87b9d036a0fa8a5
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-30 21:39:52 +0300

    copy array

---
+++ copyArray/Program.cs
@@ -0,0 +1,31 @@
+﻿var rnd = new Random();
+System.Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+int length = array.Length;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(100, 1000);
+}
+System.Console.WriteLine("Вывод исходного массива");
+
+foreach (int j in array)
+{
+    System.Console.Write(j + " ");
+}
+System.Console.WriteLine();
+
+int[] copyArray = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+    copyArray[i] = array[i];
+}
+
+System.Console.WriteLine("Вывод копии массива");
+foreach (int j in copyArray)
+{
+    System.Console.Write(j + " ");
+}
